a470c6ff599eb02a162ae4ffa833f299ca3dc212
houssem <houssemranah@gmail.com>
2022-02-22 18:25:50 +0100

    add home screen

---
+++ lib/UI/widgets/navigationbar.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+import 'package:ionicons/ionicons.dart';
+import 'package:mrigla/constants.dart';
+import 'package:salomon_bottom_bar/salomon_bottom_bar.dart';
+
+class NavigationBarHome extends StatefulWidget {
+  const NavigationBarHome({Key? key}) : super(key: key);
+
+  @override
+  _NavigationBarHomeState createState() => _NavigationBarHomeState();
+}
+
+class _NavigationBarHomeState extends State<NavigationBarHome> {
+  @override
+  Widget build(BuildContext context) {
+    return SalomonBottomBar(
+        unselectedItemColor: colorWhite,
+        selectedItemColor: colorMain,
+        items: [
+          SalomonBottomBarItem(
+              icon: Icon(Ionicons.rocket), title: Text('services')),
+          SalomonBottomBarItem(
+              icon: Icon(Ionicons.pricetags), title: Text('mes commandes')),
+          SalomonBottomBarItem(
+              icon: Icon(Ionicons.storefront), title: Text('marcheé')),
+          SalomonBottomBarItem(
+              icon: Icon(Ionicons.car), title: Text('ma voiture')),
+        ]);
+  }
+}
